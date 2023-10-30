cc3df0b94863144c93f6232cf5e493fd3eb7dd97
pg321 <priyanshi.ee19@nsut.ac.in>
2023-10-30 21:34:42 +0530

    Commit

---
+++ book_api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from book_api.views import BookCreate, BookList, BookDetail
+
+urlpatterns = [
+    path('', BookCreate.as_view()),
+    path('list/', BookList.as_view()),
+    path('<int:pk>', BookDetail.as_view())
+]
